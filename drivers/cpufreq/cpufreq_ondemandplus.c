8a42769ada2d278ec0a450cd2d761bc45cb3632c
daeiron <thomasvangerven@gmail.com>
2014-08-17 14:41:31 +0200

    Ondemandplus: Set min frequencies to 192000
    Change-Id: Ib4e57d7555165c48d93b6e228082073d7b863d10

--- drivers/cpufreq/cpufreq_ondemandplus.c
+++ drivers/cpufreq/cpufreq_ondemandplus.c
@@ -95,7 +95,7 @@ static unsigned long up_threshold;
 #define DEFAULT_DOWN_DIFFERENTIAL 20
 static unsigned long down_differential;
 
-#define DEFAULT_MIN_FREQ 384000
+#define DEFAULT_MIN_FREQ 192000
 static u64 allowed_min;
 
 #define DEFAULT_MAX_FREQ 1512000
@@ -107,7 +107,7 @@ static u64 inter_hifreq;
 #define DEFAULT_INTER_LOFREQ 384000
 static u64 inter_lofreq;
 
-#define SUSPEND_FREQ 384000
+#define SUSPEND_FREQ 192000
 static u64 suspend_frequency;
 
 #define DEFAULT_INTER_STAYCYCLES 2
