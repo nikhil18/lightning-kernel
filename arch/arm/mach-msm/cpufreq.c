a8b117dbd3e4510d3262bdf5d4849a4aee148aeb
hellsgod <hellsgod@gmx.ch>
2014-09-08 19:13:50 +0200

    msm-sleeper: save previous max freq, add ex_max_freq interface, loosen permissions
    credits to flar2
    
    Conflicts:
    	arch/arm/mach-msm/cpufreq.c

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -36,6 +36,8 @@
 /* maxscroff */
 uint32_t maxscroff_freq = 1026000;
 uint32_t maxscroff = 1;
+/* ex max freq */
+uint32_t ex_max_freq;
 #endif
 
 struct cpufreq_work_struct {
@@ -319,6 +321,7 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 				policy->cpu, cur_freq);
 		return -EINVAL;
 	}
+<<<<<<< HEAD
 
 	if (cur_freq != table[index].frequency) {
 		int ret = 0;
@@ -332,6 +335,19 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	}
 
 	policy->cur = cur_freq;
+=======
+	/*
+	 * Call set_cpu_freq unconditionally so that when cpu is set to
+	 * online, frequency limit will always be updated.
+	 */
+	ret = set_cpu_freq(policy, table[index].frequency, table[index].index);
+	if (ret)
+		return ret;
+	pr_debug("cpufreq: cpu%d init at %d switching to %d\n",
+			policy->cpu, cur_freq, table[index].frequency);
+
+	policy->cur = table[index].frequency;
+>>>>>>> 01236d2... msm-sleeper: save previous max freq, add ex_max_freq interface, loosen permissions
 
 	policy->cpuinfo.transition_latency =
 		acpuclk_get_switch_time() * NSEC_PER_USEC;
@@ -401,8 +417,63 @@ static int msm_cpufreq_resume(struct cpufreq_policy *policy)
 }
 
 #ifdef CONFIG_MSM_SLEEPER
-/** maxscreen off sysfs interface **/
+/** max freq interface **/
 
+static ssize_t show_ex_max_freq(struct cpufreq_policy *policy, char *buf)
+{
+	if (!ex_max_freq)
+		ex_max_freq = policy->max;
+
+	return sprintf(buf, "%u\n", ex_max_freq);
+}
+
+static ssize_t store_ex_max_freq(struct cpufreq_policy *policy,
+		const char *buf, size_t count)
+{
+	unsigned int freq = 0;
+	int ret, cpu;
+	int index;
+	struct cpufreq_frequency_table *freq_table = cpufreq_frequency_get_table(policy->cpu);
+
+	if (!freq_table)
+		return -EINVAL;
+
+	ret = sscanf(buf, "%u", &freq);
+	if (ret != 1)
+		return -EINVAL;
+
+	mutex_lock(&per_cpu(cpufreq_suspend, policy->cpu).suspend_mutex);
+
+	ret = cpufreq_frequency_table_target(policy, freq_table, freq,
+			CPUFREQ_RELATION_H, &index);
+	if (ret)
+		goto out;
+
+	ex_max_freq = freq_table[index].frequency;
+
+	for_each_possible_cpu(cpu) {
+		msm_cpufreq_set_freq_limits(cpu, MSM_CPUFREQ_NO_LIMIT, ex_max_freq);
+	}
+	cpufreq_update_policy(cpu);
+
+	ret = count;
+
+out:
+	mutex_unlock(&per_cpu(cpufreq_suspend, policy->cpu).suspend_mutex);
+	return ret;
+}
+
+struct freq_attr msm_cpufreq_attr_ex_max_freq = {
+	.attr = { .name = "ex_max_freq",
+		.mode = 0666,
+	},
+	.show = show_ex_max_freq,
+	.store = store_ex_max_freq,
+};
+/** end max freq interface **/
+
+
+/** maxscreen off sysfs interface **/
 static ssize_t show_max_screen_off_khz(struct cpufreq_policy *policy, char *buf)
 {
 	return sprintf(buf, "%u\n", maxscroff_freq);
@@ -475,6 +546,14 @@ struct freq_attr msm_cpufreq_attr_max_screen_off = {
 
 static struct freq_attr *msm_freq_attr[] = {
 	&cpufreq_freq_attr_scaling_available_freqs,
+<<<<<<< HEAD
+=======
+#ifdef CONFIG_MSM_SLEEPER
+	&msm_cpufreq_attr_max_screen_off_khz,
+	&msm_cpufreq_attr_max_screen_off,
+	&msm_cpufreq_attr_ex_max_freq,
+#endif
+>>>>>>> 01236d2... msm-sleeper: save previous max freq, add ex_max_freq interface, loosen permissions
 	NULL,
 };
 
