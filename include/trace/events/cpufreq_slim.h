97280598ea9c933efa53bd93414c858d469f9058
nikhil18 <nikhilgolechha1@gmail.com>
2015-06-09 13:23:40 +0530

    add new governors

--- include/trace/events/cpufreq_slim.h
+++ include/trace/events/cpufreq_slim.h
@@ -1,5 +1,5 @@
 #undef TRACE_SYSTEM
-#define TRACE_SYSTEM cpufreq_interactive
+#define TRACE_SYSTEM cpufreq_slim
 
 #if !defined(_TRACE_CPUFREQ_INTERACTIVE_H) || defined(TRACE_HEADER_MULTI_READ)
 #define _TRACE_CPUFREQ_INTERACTIVE_H
@@ -28,9 +28,15 @@ DECLARE_EVENT_CLASS(set,
 	      __entry->actualfreq)
 );
 
-DEFINE_EVENT(set, cpufreq_interactive_setspeed,
+DEFINE_EVENT(set, cpufreq_slim_up,
 	TP_PROTO(u32 cpu_id, unsigned long targfreq,
-	     unsigned long actualfreq),
+		unsigned long actualfreq),
+	TP_ARGS(cpu_id, targfreq, actualfreq)
+);
+
+DEFINE_EVENT(set, cpufreq_slim_down,
+	TP_PROTO(u32 cpu_id, unsigned long targfreq,
+		unsigned long actualfreq),
 	TP_ARGS(cpu_id, targfreq, actualfreq)
 );
 
@@ -61,51 +67,19 @@ DECLARE_EVENT_CLASS(loadeval,
 		      __entry->curactual, __entry->newtarg)
 );
 
-DEFINE_EVENT(loadeval, cpufreq_interactive_target,
+DEFINE_EVENT(loadeval, cpufreq_slim_target,
 	    TP_PROTO(unsigned long cpu_id, unsigned long load,
 		     unsigned long curtarg, unsigned long curactual,
 		     unsigned long newtarg),
 	    TP_ARGS(cpu_id, load, curtarg, curactual, newtarg)
 );
 
-DEFINE_EVENT(loadeval, cpufreq_interactive_already,
+DEFINE_EVENT(loadeval, cpufreq_slim_already,
 	    TP_PROTO(unsigned long cpu_id, unsigned long load,
 		     unsigned long curtarg, unsigned long curactual,
 		     unsigned long newtarg),
 	    TP_ARGS(cpu_id, load, curtarg, curactual, newtarg)
 );
-
-DEFINE_EVENT(loadeval, cpufreq_interactive_notyet,
-	    TP_PROTO(unsigned long cpu_id, unsigned long load,
-		     unsigned long curtarg, unsigned long curactual,
-		     unsigned long newtarg),
-	    TP_ARGS(cpu_id, load, curtarg, curactual, newtarg)
-);
-
-TRACE_EVENT(cpufreq_interactive_boost,
-	    TP_PROTO(const char *s),
-	    TP_ARGS(s),
-	    TP_STRUCT__entry(
-		    __string(s, s)
-	    ),
-	    TP_fast_assign(
-		    __assign_str(s, s);
-	    ),
-	    TP_printk("%s", __get_str(s))
-);
-
-TRACE_EVENT(cpufreq_interactive_unboost,
-	    TP_PROTO(const char *s),
-	    TP_ARGS(s),
-	    TP_STRUCT__entry(
-		    __string(s, s)
-	    ),
-	    TP_fast_assign(
-		    __assign_str(s, s);
-	    ),
-	    TP_printk("%s", __get_str(s))
-);
-
 #endif /* _TRACE_CPUFREQ_INTERACTIVE_H */
 
 /* This part must be outside protection */
